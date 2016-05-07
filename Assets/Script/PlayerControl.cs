6035d8bffdbadd02bfaa2a618c6b3f84204616b8
kerrot <kerrot.gs@gmail.com>
2016-05-07 14:31:05 +0900

    Init

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+    public WireControl hitWire;
+    public bool IsMoving { get { return !isStop; } }
+
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private WireControl wire1;
+    private WireControl wire2;
+    private bool isStop = true;
+    private Vector2 target;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+
+        wire1 = transform.GetChild(0).GetComponent<WireControl>();
+        wire2 = transform.GetChild(1).GetComponent<WireControl>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (wire1.IsWiring)
+            {
+                wire2.ShootWire();
+            }
+            else
+            {
+                wire1.ShootWire();
+            }
+        }
+    }
+
+    public void SetTarget(Vector2 t)
+    {
+        target = t;
+        isStop = false;
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        if (!isStop)
+        {
+            Vector2 direction = target - (Vector2)transform.position;
+            if (direction.magnitude > 0.6)
+            {
+                rb2d.MovePosition(rb2d.position + direction.normalized * speed * Time.deltaTime);
+            }
+            else
+            {
+                isStop = true;
+                hitWire = null;
+            }
+        }
+    }
+}
---
+++ Assets/Script/WireControl.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WireControl : MonoBehaviour {
+
+    public float wireX;
+    public float wireY;
+    public int offSet;
+
+    public float WireMaxLength;
+    public float WireTime;
+
+    public bool IsWiring { get { return line.enabled; } }
+
+    private LineRenderer line;
+    private int layerMask;
+
+    private bool isReached = false;
+    private bool isHit = false;
+    private float wireCounter = 0;
+    private GameObject player;
+    private PlayerControl playerCtrl;
+    private Vector3 target;
+
+    // Use this for initialization
+    void Start () {
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+        layerMask = 1 << LayerMask.NameToLayer("Hittable");
+        player = transform.parent.gameObject;
+        playerCtrl = player.GetComponent<PlayerControl>();
+    }
+
+    public void ShootWire()
+    {
+        if (!line.enabled)
+        {
+            target = ComputeWireTarget();
+            line.enabled = true;
+            LineReset();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (line.enabled)
+        {
+            if (isReached)
+            {
+                if (playerCtrl.hitWire == this)
+                {
+                    DrawWire(target);
+                    line.enabled = playerCtrl.IsMoving;
+                }
+                else
+                {
+                    wireCounter -= Time.deltaTime;
+                    float rate = wireCounter / WireTime;
+                    if (wireCounter < 0)
+                    {
+                        line.enabled = false;
+                    }
+                    DrawWire((target - player.transform.position) * rate + player.transform.position);
+                }
+            }
+            else
+            {
+                wireCounter += Time.deltaTime;
+                float rate = wireCounter / WireTime;
+                if (wireCounter > WireTime)
+                {
+                    rate = 1;
+                    wireCounter = WireTime;
+                    isReached = true;
+                    if (isHit)
+                    {
+                        playerCtrl.SetTarget(target);
+                        playerCtrl.hitWire = this;
+                    }
+                }
+                DrawWire((target - player.transform.position) * rate + player.transform.position);
+            }
+        }
+    }
+
+    void LineReset()
+    {
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, Vector3.zero);
+        wireCounter = 0;
+        isReached = false;
+    }
+
+    void DrawWire(Vector2 vec)
+    {
+        Vector2 tmp = player.transform.position;
+        line.SetPosition(0, tmp);
+        line.SetPosition(1, vec);
+        line.material.mainTextureScale = new Vector2((vec - tmp).magnitude, 1);
+    }
+
+    Vector2 ComputeScreenPosition()
+    {
+        Vector3 position = Input.mousePosition;
+
+        float l = wireX * ((float)(position.x - Screen.width / 2)) / ((float)(Screen.width / 2));
+        float w = wireY * ((float)(position.y - Screen.height / 2 - offSet)) / ((float)(Screen.height / 2));
+
+        return new Vector2(l, w);
+    }
+
+    Vector3 ComputeWireTarget()
+    {
+        Vector2 direction = ComputeScreenPosition();
+        Vector2 tmpTarget;
+        RaycastHit2D hit = Physics2D.Raycast(player.transform.position, direction, WireMaxLength, layerMask);
+
+        if (hit.collider != null)
+        {
+            isHit = true;
+            tmpTarget = hit.point;
+        }
+        else
+        {
+            tmpTarget = (Vector2)player.transform.position + direction.normalized * WireMaxLength;
+            isHit = false;
+        }
+
+        return new Vector3(tmpTarget.x, tmpTarget.y, 0);
+    }
+}
