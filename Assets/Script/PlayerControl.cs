7859f6a847643ed5cf0aeba41916657e0704babd
iOS <iOS@MacBook-Pro-04.local>
2016-06-04 13:33:52 +0900

    a

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -49,4 +49,12 @@ public class PlayerControl : MonoBehaviour {
             PlayerImage.transform.localScale = new Vector3((direction.x > 0 ? -1 : 1), 1, 1);
         }
     }
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "enemy") {
+			Destroy (gameObject);
+		}
+
+	}
+
+			
 }
