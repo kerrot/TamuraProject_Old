9b486cc8a1d7ea1e7befaf46c192eb39b699104d
kin <tetares4@gmail.com>
2016-09-10 15:08:52 +0900

    movie title

--- Assets/Resources/Script/System/MoviePlay.cs
+++ Assets/Resources/Script/System/MoviePlay.cs
@@ -2,13 +2,25 @@
 using System.Collections;
 
 public class MoviePlay : MonoBehaviour {
-	/*
-	MovieTexture movie;
 
-	void Start ()
+
+
+	void Update () 
 	{
-		movie = ((MovieTexture)GetComponent<Renderer> ().material.mainTexture);
-		movie.loop = true;
-		movie.Play();
-    }*/
+		InvokeRepeating("LaunchProjectile",1,5);
+	}
+
+	void LaunchProjectile ()
+	{
+		Handheld.PlayFullScreenMovie("qwe.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+	}
+
+	void OnGUI(){
+		if (GUILayout.Button("オープンニング", GUILayout.Height(150)))
+		{
+			Handheld.PlayFullScreenMovie("qwe.mp4", Color.white, FullScreenMovieControlMode.CancelOnInput);
+		}
+	}
+
+
 }
